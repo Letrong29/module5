e88dca88c1e553d3efe1703ba3249c1fdfb31fd6
letrong29 <letrong2929Gmail.com>
2022-09-24 15:44:23 +0700

    component and template

---
+++ ss4_component_and_template/practice/angular-hackernews-app/src/app/like/like.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-like',
+  templateUrl: './like.component.html',
+  styleUrls: ['./like.component.css']
+})
+export class LikeComponent implements OnInit {
+
+  like = 0;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  likeThis() {
+    if (this.like === 0) {
+      this.like++;
+    } else {
+      this.like--;
+    }
+  }
+
+}
