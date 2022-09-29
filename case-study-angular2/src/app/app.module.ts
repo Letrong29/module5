87a5dd08a3c4245a4e1424590c64142d711ed05a
letrong29 <letrong2929Gmail.com>
2022-09-29 21:03:37 +0700

    angular form

--- case-study-angular2/src/app/app.module.ts
+++ case-study-angular2/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ServiceListComponent } from './service-list/service-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ServiceCreateComponent } from './service-create/service-create.component';
+import { ServiceEditComponent } from './service-edit/service-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ServiceListComponent,
+    FooterComponent,
+    HomeComponent,
+    ServiceCreateComponent,
+    ServiceEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ ss6_angular_form/exercise/angular-form/src/app/register/register.component.ts
@@ -0,0 +1,56 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, ValidationErrors} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  countryList: Array<string> = ['Vietnam', 'USA', 'Korea', 'Japan'];
+
+  constructor() {
+    this.registerForm = new FormGroup({
+
+      email: new FormControl('', [Validators.required, Validators.email]),
+
+      passwordGroup: new FormGroup({
+        password: new FormControl('',
+          [Validators.required,
+          Validators.minLength(6)]),
+
+        passwordConfirm: new FormControl()
+      }, [this.checkPassword]),
+
+      country: new FormControl('', [Validators.required]),
+
+      age: new FormControl('', [Validators.required, Validators.min(18)]),
+
+      gender: new FormControl('', [Validators.required]),
+
+      phone: new FormControl('', [Validators.pattern(/^\+84\d{9,10}$/)]),
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.countryList);
+  }
+
+  private checkPassword(passwordGroup: AbstractControl) {
+
+    let password = passwordGroup.get('password').value;
+
+    let passwordConfirm = passwordGroup.get('passwordConfirm').value;
+
+    if (password !== passwordConfirm) {
+      return {'checkPasswordConfirm': true};
+    }
+
+    return null;
+  }
+}
